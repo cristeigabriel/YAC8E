24308265c1e5e0ee9556b7924edb3dbacd5dbaeb
Gabriel <51248378+cristeigabriel@users.noreply.github.com>
2021-03-04 15:44:52 +0200

    [chip-8] Fixed logging, made instructions more apparent, implemented N PC, N SP, N S[SP], N OPERAND in logging after instruction

--- chip-8/chip-8.cpp
+++ chip-8/chip-8.cpp
@@ -18,20 +18,21 @@ void CChip8<>::ComputeInstruction() {
 	BYTE x = GET_X(m_opcode);
 	BYTE y = GET_Y(m_opcode);
 
-	LOG("C PC: 0x%X: C SP: 0x%X C S[SP]: 0x%X C OP: 0x%X | ", m_program_counter,
+	LOG("C PC: 0x%X: C SP: 0x%X C S[SP]: 0x%X C OP: 0x%X | <<", m_program_counter,
 		m_stack_pointer, m_stack[m_stack_pointer], m_opcode);
+
 	switch (INVERSE_NNN(m_opcode)) {
 	case 0x0000:
 		switch (m_opcode) {
 		case CLS:
-			LOG_NO_FMT("CLS\n");
+			LOG_NO_FMT("CLS");
 
 			//	Clear graphics buffer
 			memset(m_graphics, 0, sizeof(m_graphics));
 			break;
 
 		case RET:
-			LOG_NO_FMT("RET\n");
+			LOG_NO_FMT("RET");
 
 			if (m_stack[m_stack_pointer] > 0) {
 				assert("Invalid return");
@@ -53,7 +54,7 @@ void CChip8<>::ComputeInstruction() {
 
 	case 0x1000:
 		//	Jump to address
-		LOG("JP, 0x%X\n", GET_NNN(m_opcode));
+		LOG("JP, 0x%X", GET_NNN(m_opcode));
 
 		//	Check if we're in jump to avoid incrementing within a jump, thus 
 		//	causing trouble within the program flow
@@ -65,7 +66,7 @@ void CChip8<>::ComputeInstruction() {
 
 	case 0x2000:
 		//	Call address
-		LOG("CALL, 0x%X\n", GET_NNN(m_opcode));
+		LOG("CALL, 0x%X", GET_NNN(m_opcode));
 
 		if (m_stack[m_stack_pointer] != 0) {
 			//	Increment the stack pointer
@@ -84,7 +85,7 @@ void CChip8<>::ComputeInstruction() {
 
 	case 0x3000:
 		//	Skip next instruction if Vx == kk
-		LOG("SE V%X, 0x%X\n", m_general_purpose_registers[x], GET_KK(m_opcode));
+		LOG("SE V%X, 0x%X", x, GET_KK(m_opcode));
 
 		//	Compare register Vx to kk, and if they are equal, increments the program counter by 2.
 		if (m_general_purpose_registers[x] & GET_KK(m_opcode)) {
@@ -94,7 +95,7 @@ void CChip8<>::ComputeInstruction() {
 
 	case 0x4000:
 		//	Skip next instruction if Vx != kk
-		LOG("SNE V%X, 0x%X\n", m_general_purpose_registers[x], GET_KK(m_opcode));
+		LOG("SNE V%X, 0x%X", x, GET_KK(m_opcode));
 
 		//	Compare register Vx to kk, and if they are not equal, increments the program counter by 2.
 		if (!(m_general_purpose_registers[x] & GET_KK(m_opcode))) {
@@ -104,7 +105,7 @@ void CChip8<>::ComputeInstruction() {
 		
 	case 0x5000:
 		//	Skip next instruction if Vx == Vy
-		LOG("SE V%X, V%X\n", m_general_purpose_registers[x], m_general_purpose_registers[y]);
+		LOG("SE V%X, V%X", x, y);
 
 		//	Compare register Vx to kk, and if they are not equal, increments the program counter by 2.
 		if (m_general_purpose_registers[x] & m_general_purpose_registers[y]) {
@@ -114,14 +115,14 @@ void CChip8<>::ComputeInstruction() {
 
 	case 0x6000:
 		//	Set Vx = kk
-		LOG("LD V%X, %X\n", m_general_purpose_registers[x], GET_KK(m_opcode));
+		LOG("LD V%X, %X", x, GET_KK(m_opcode));
 
 		m_general_purpose_registers[x] = GET_KK(m_opcode);
 		break;
 
 	case 0x7000:
 		//	Set Vx = Vx + kk
-		LOG("ADD V%X, %X\n", m_general_purpose_registers[x], GET_KK(m_opcode));
+		LOG("ADD V%X, %X", x, GET_KK(m_opcode));
 
 		m_general_purpose_registers[x] += GET_KK(m_opcode);
 		break;
@@ -130,35 +131,35 @@ void CChip8<>::ComputeInstruction() {
 		switch (GET_N(m_opcode)) {
 		case 0x0:
 			//	Set Vx = Vy
-			LOG("LD V%X, V%X\n", m_general_purpose_registers[x], m_general_purpose_registers[y]);
+			LOG("LD V%X, V%X", x, y);
 
 			m_general_purpose_registers[x] = m_general_purpose_registers[y];
 			break;
 
 		case 0x1:
 			//	Set Vx = Vx OR Vy
-			LOG("OR V%X, V%X\n", m_general_purpose_registers[x], m_general_purpose_registers[y]);
+			LOG("OR V%X, V%X", x, y);
 
 			m_general_purpose_registers[x] |= m_general_purpose_registers[y];
 			break;
 
 		case 0x2:
 			//	Set Vx = Vx AND Vy
-			LOG("AND V%X, V%X\n", m_general_purpose_registers[x], m_general_purpose_registers[y]);
+			LOG("AND V%X, V%X", x, y);
 
 			m_general_purpose_registers[x] &= m_general_purpose_registers[y];
 			break;
 
 		case 0x3:
 			//	Set Vx = Vx XOR Vy
-			LOG("XOR V%X, V%X\n", m_general_purpose_registers[x], m_general_purpose_registers[y]);
+			LOG("XOR V%X, V%X", x, y);
 
 			m_general_purpose_registers[x] ^= m_general_purpose_registers[y];
 			break;
 
 		case 0x4:
 			//	Set Vx = Vx + Vy, set VF = carry
-			LOG("ADD V%X, V%X\n", m_general_purpose_registers[x], m_general_purpose_registers[y]);
+			LOG("ADD V%X, V%X", x, y);
 
 			//	Vx = Vx + Vy
 			m_general_purpose_registers[x] = m_general_purpose_registers[x] + m_general_purpose_registers[y];
@@ -170,7 +171,7 @@ void CChip8<>::ComputeInstruction() {
 
 		case 0x5:
 			//	Set Vx = Vx - Vy, set VF = NOT borrow.
-			LOG("SUB V%X, V%X\n", m_general_purpose_registers[x], m_general_purpose_registers[y]);
+			LOG("SUB V%X, V%X", x, y);
 
 			//	If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted from Vx, and the results stored in Vx.
 			m_general_purpose_registers[0xF] = (int)(m_general_purpose_registers[x] > m_general_purpose_registers[y]);
@@ -180,7 +181,7 @@ void CChip8<>::ComputeInstruction() {
 
 		case 0x6:
 			//	Set Vx = Vx SHR 1
-			LOG("SHR V%X{, V%X}\n", m_general_purpose_registers[x], m_general_purpose_registers[y]);
+			LOG("SHR V%X{, V%X}", x, y);
 
 			//	If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted from Vx, and the results stored in Vx.
 			m_general_purpose_registers[0xF] = (int)(m_general_purpose_registers[x] % 2 == 1);
@@ -190,7 +191,7 @@ void CChip8<>::ComputeInstruction() {
 
 		case 0x7:
 			//	Set Vx = Vy - Vx, set VF = NOT borrow
-			LOG("SUBN V%X, V%X\n", m_general_purpose_registers[x], m_general_purpose_registers[y]);
+			LOG("SUBN V%X, V%X", x, y);
 
 			//	 If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is subtracted from Vy, and the results stored in Vx.
 			m_general_purpose_registers[0xF] = (int)(m_general_purpose_registers[y] > m_general_purpose_registers[x]);
@@ -200,7 +201,7 @@ void CChip8<>::ComputeInstruction() {
 
 		case 0xE:
 			//	Set Vx = Vx SHL 1
-			LOG("SHL V%X{, V%X}\n", m_general_purpose_registers[x], m_general_purpose_registers[y]);
+			LOG("SHL V%X{, V%X}", x, y);
 
 			//	If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2.
 			m_general_purpose_registers[0xF] = (int)((m_general_purpose_registers[y] > 128) == 1);
@@ -215,7 +216,7 @@ void CChip8<>::ComputeInstruction() {
 
 	case 0x9000:
 		//	Skip next instruction if Vx != Vy.
-		LOG("SNE V%X, V%X\n", m_general_purpose_registers[x], m_general_purpose_registers[y]);
+		LOG("SNE V%X, V%X", x, y);
 		
 		if (!(m_general_purpose_registers[x] & m_general_purpose_registers[y])) {
 			m_program_counter += 2;
@@ -225,6 +226,9 @@ void CChip8<>::ComputeInstruction() {
 	default:
 		break;
 	}
+
+	LOG(">> | N PC: 0x%X: N SP: 0x%X N S[SP]: 0x%X N OP: 0x%X\n", m_program_counter,
+		m_stack_pointer, m_stack[m_stack_pointer], m_opcode);
 
 	if (!jump) {
 		//	Advance in the program
